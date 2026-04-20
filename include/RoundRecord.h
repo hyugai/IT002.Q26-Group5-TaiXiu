dc4627045f927b0b6b875fa932364f7fc7ad346d
QuocCong <quoccong2810@gmail.com>
2026-04-20 10:31:35 +0700

    fix

--- include/RoundRecord.h
+++ include/RoundRecord.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "ABettingStrategy.h"
 struct RoundRecord {
-    Bet       bet;              /* Cược đặt trong ván (cửa Tài/Xỉu và số tiền) */
-    BetResult result;           /* Kết quả ván: Win hoặc Lose */
-    double    dCurrentBankroll; /* Số dư sau khi ván kết thúc */
+    Bet       bet;              
+    BetResult result;           
+    double    dCurrentBankroll; 
 
     RoundRecord(Bet b, BetResult r, double bankroll)
         : bet(b), result(r), dCurrentBankroll(bankroll) {}
